6919eca3ae43bfa9ce37c4e889b919bc2f7a914a
Christie Lynam <christielynam@gmail.com>
2019-07-14 12:45:08 -0600

    Add node-scss and fonts

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import './styles.scss'
+import wordcloudMan from '../../assets/wordcloud_man.png'
 
 const LandingPage = () => {
   return (
-    <div>
-      This is the Landing Page
-    </div>
+    <div className='landing-page'>
+      <section className='hero-section'>
+        <header className='landing-header'>
+          <h1 className='main-title'>do you.</h1>
+          <h3 className='main-tagline'>let your Personality shine.</h3>
+        </header>
+        <article className='mission-article'>
+          <div className='wordcloud-container'>
+            <img className='wordcloud-man' src={wordcloudMan} alt='wordcloud man' />
+          </div>
+          <div className='mission-statement'>
+            <h6 className='purpose'>Recognizing and understanding your personality type can change the way you see yourself - which in turns affects everything you do and every aspect of your life. Please take any of our personality assessments to help you unlock your potential, find your passion, and DO YOU!</h6>
+          </div>
+        </article>
+      </section>
+      <section className='lightbulb-section'>
+        {/* <Lightbulb bulbNum={'bulb-one'} text={'sign up'} route={'/signup'} />
+        <Lightbulb bulbNum={'bulb-two'} text={'personalities'} route={'/personality-types'}/>
+        <Lightbulb bulbNum={'bulb-three'} text={'login'} route={'/login'} />
+        <Lightbulb bulbNum={'bulb-four'} text={'assessments'} route={'/assessments'} /> */}
+    </section>
+  </div>
+    
   )
 }
 
